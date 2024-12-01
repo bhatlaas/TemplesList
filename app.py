b83a1078fbc696ef9a7fef41c2307e68d75d9c72
bhatlaas <ashok.bhatla.writer@gmail.com>
2024-11-30 20:37:14 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def create_map():
         ).add_to(mymap)
 
     # Save the map to an HTML file
-    map_path = "templates/map.html"
+    map_path = "map.html"
     mymap.save(map_path)
 
 # Route for the homepage
